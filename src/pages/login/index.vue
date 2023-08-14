67f831f3922e6435ba9141e20e77f80873896727
NJR10byh <njr10byh@gmail.com>
2023-08-14 15:40:16 +0800

    登录页样式问题修复

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -4,31 +4,28 @@
       <div class="title">
         智慧报单系统
       </div>
-      <div>
-        <t-form
-            ref="loginForm"
-            :data="loginFormData"
-            :rules="loginFormDataRules"
-            reset-type="initial"
-            show-error-message
-            label-align="left"
-            @submit="onSubmit"
-            colon
-            :requiredMark="false"
-            style="margin-top: 20px;"
-        >
-          <t-form-item label="手机号" name="username">
-            <t-input v-model="loginFormData.username" borderless placeholder="请输入手机号"/>
-          </t-form-item>
-          <t-form-item label="密码" name="password">
-            <t-input v-model="loginFormData.password" borderless type="password" placeholder="请输入密码"/>
-          </t-form-item>
-          <t-button theme="primary" block size="large" type="submit" :loading="loginBtnLoading"
-                    style="margin-top: 20px;">
-            登 录
-          </t-button>
-        </t-form>
-      </div>
+      <t-form
+          ref="loginForm"
+          :data="loginFormData"
+          :rules="loginFormDataRules"
+          reset-type="initial"
+          show-error-message
+          label-align="left"
+          @submit="onSubmit"
+          colon
+          :requiredMark="false"
+          class="formStyle"
+      >
+        <t-form-item label="手机号" name="username">
+          <t-input v-model="loginFormData.username" borderless placeholder="请输入手机号"/>
+        </t-form-item>
+        <t-form-item label="密码" name="password">
+          <t-input v-model="loginFormData.password" borderless type="password" placeholder="请输入密码"/>
+        </t-form-item>
+        <div class="button-group">
+          <t-button theme="primary" type="submit" :loading="loginBtnLoading">登 录</t-button>
+        </div>
+      </t-form>
     </div>
   </div>
 </template>
@@ -106,6 +103,20 @@ const onSubmit = async ({validateResult}) => {
       font-size: 23px;
       font-weight: bold;
       color: var(--td-brand-color-8);
+    }
+
+    .formStyle {
+      width: 90%;
+      border-radius: 10px;
+      margin-top: 10px;
+
+      .button-group {
+        padding: 10px;
+
+        .t-button {
+          width: 100%;
+        }
+      }
     }
   }
 }
